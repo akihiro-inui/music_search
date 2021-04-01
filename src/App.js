14b0769f2ad64a41b12c3a30ec2e10c9e18e87c6
inuinana <mail@akihiroinui.com>
2021-04-01 22:17:13 +0200

    Add example search

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import {
+    ReactiveBase,
+    CategorySearch,
+    SingleRange,
+    ResultCard,
+    ReactiveList,
+} from '@appbaseio/reactivesearch';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <ReactiveBase
+                app="carstore-dataset"
+                credentials="4HWI27QmA:58c731f7-79ab-4f55-a590-7e15c7e36721"
+            >
+                <CategorySearch
+                    componentId="searchbox"
+                    dataField="model"
+                    categoryField="brand.keyword"
+                    placeholder="Search for cars"
+                />
+                <SingleRange
+                    componentId="ratingsfilter"
+                    title="Filter by ratings"
+                    dataField="rating"
+                    data={[
+                        { start: '4', end: '5', label: '4 stars and up' },
+                        { start: '3', end: '5', label: '3 stars and up' },
+                        { start: '2', end: '5', label: '2 stars and up' },
+                        { start: '1', end: '5', label: 'see all ratings' },
+                    ]}
+                    defaultValue="4 stars and up"
+                />
+                <ReactiveList
+                    componentId="result"
+                    title="Results"
+                    dataField="model"
+                    from={0}
+                    size={5}
+                    pagination={true}
+                    react={{
+                        and: ['searchbox', 'ratingsfilter'],
+                    }}
+                    render={({ data }) => (
+                        <ReactiveList.ResultCardsWrapper>
+                            {data.map(item => (
+                                <ResultCard key={item._id}>
+                                    <ResultCard.Image src="https://bit.do/demoimg" />
+                                    <ResultCard.Title
+                                        dangerouslySetInnerHTML={{
+                                            __html: item.model,
+                                        }}
+                                    />
+                                    <ResultCard.Description>
+                                        {item.brand + ' ' + '*'.repeat(item.rating)}
+                                    </ResultCard.Description>
+                                </ResultCard>
+                            ))}
+                        </ReactiveList.ResultCardsWrapper>
+                    )}
+                />
+            </ReactiveBase>
+        );
+    }
 }
 
 export default App;
+
